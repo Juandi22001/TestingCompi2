5b7932f9b99a2922e73582e06c18f112e68fc18e
Juandi2200 <57282878+Juandi22001@users.noreply.github.com>
2021-06-14 23:13:01 -0600

    de

--- electronjs.org-master/middleware/webpack.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack')
-const config = require('../webpack.common')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-
-module.exports = () => {
-  return webpackDevMiddleware(webpack(config), {
-    publicPath: config.output.publicPath,
-  })
-}
--- templates/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  app: require('./app.hbs'),
-  api: require('./api.hbs'),
-  package: require('./package.hbs'),
-  tutorial: require('./tutorial.hbs'),
-}
